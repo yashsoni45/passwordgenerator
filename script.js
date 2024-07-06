1a2fc9adc3df1036e6e014e0facfc41d9d7b365b
yashsoni45 <yashraj4549@gmail.com>
2024-07-06 14:59:35 +0530

    updated

--- script.js
+++ script.js
@@ -23,11 +23,15 @@ handlSlider();
 function handlSlider(){
     inputSlider.value=passwordLength;
     lengthDisplay.innerText=passwordLength;
+    const min = inputSlider.min;
+    const max= inputSlider.max;
+    inputSlider.style.backgroundSize=((passwordLength-min)*100/(max-min))+"% 100%";
 }
 
 function setIndicator(color){
     indicator.style.backgroundColor=color;
     //shadow
+    indicator.style.boxShadow=`0px 0px 12px 1px ${color}`;
 }
 
 function getRndInteger(min, max){
